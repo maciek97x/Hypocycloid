298bab18c72c1dc117b3d829ea8c886c712115d8
maciek97x <maciek97x@o2.pl>
2020-11-03 11:31:53 +0100

    final

--- Hypocycloid/HypocycloidCurve.cs
+++ Hypocycloid/HypocycloidCurve.cs
@@ -3,44 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Hypocycloid
 {
     class Point
     {
+        public double x;
+        public double y;
+
         public Point(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
-        public double x;
-        public double y;
+
+        public Point Copy()
+        {
+            return new Point(x, y);
+        }
     }
 
     enum State
     {
         STOP,
         RUNNING,
-        PAUSE
+        PAUSE,
+        END
     }
 
     class HypocycloidCurve
     {
         public double R { get; set; }
+        public Point O { get; private set; }
         public double r { get; set; }
+        public Point o { get; private set; }
 
         public double t { get; private set; }
         public Point alpha { get; private set; }
 
         public List<Point> trail { get; private set; }
 
-        private double t_0;
+        public double speed { get; set; }
+        
         private State state;
+        private double deltaTime;
+        private double time;
 
         public HypocycloidCurve()
         {
             trail = new List<Point>();
+            alpha = new Point(0, 0);
+            speed = 1.0;
             t = 0.0;
+            O = new Point(0, 0);
+            o = new Point(0, 0);
+            time = DateTime.Now.Ticks / TimeSpan.TicksPerSecond;
             state = State.STOP;
         }
 
@@ -48,15 +66,15 @@ namespace Hypocycloid
         {
             trail = new List<Point>();
             t = 0.0;
+            time = DateTime.Now.Ticks / TimeSpan.TicksPerSecond;
             state = State.STOP;
         }
 
         public void Start()
         {
-            if (state == State.STOP)
-            {
-                t_0 = DateTime.Now.Ticks / TimeSpan.TicksPerSecond;
-            }
+            double now = ((double)DateTime.Now.Ticks) / ((double)TimeSpan.TicksPerSecond);
+            deltaTime = now - time;
+            time = now;
             state = State.RUNNING;
         }
 
@@ -65,13 +83,22 @@ namespace Hypocycloid
             state = State.PAUSE;
         }
 
-        public void CalculateStep()
+        public void Compute()
         {
-            alpha = new Point(
-                (R - r) * Math.Cos(t) + r * Math.Cos(t * (R - r) / r),
-                (R - r) * Math.Sin(t) - r * Math.Sin(t * (R - r) / r));
-            trail.Add(alpha);
+            double now = ((double)DateTime.Now.Ticks) / ((double)TimeSpan.TicksPerSecond);
+            deltaTime = now - time;
+            time = now;
+            if (state == State.RUNNING)
+            {
+                o.x = (R - r) * Math.Cos(t);
+                o.y = (R - r) * Math.Sin(t);
+
+                alpha.x = o.x + r * Math.Cos(t * (R - r) / r);
+                alpha.y = o.y - r * Math.Sin(t * (R - r) / r);
+
+                trail.Add(alpha.Copy());
+                t += deltaTime * speed;
+            }
         }
-        
     }
 }
--- Hypocycloid/MainWindow.xaml.cs
+++ Hypocycloid/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace Hypocycloid
 {
@@ -22,11 +26,206 @@ namespace Hypocycloid
     {
         private HypocycloidCurve curve;
 
+        private Ellipse outerCircle;
+        private Ellipse innerCircle;
+        private Ellipse alpha;
+        private List<Line> trail;
+
+        private Point canvasMid;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            speedTextBox.Text = "1.0";
+            StartButton.IsEnabled = false;
+
             curve = new HypocycloidCurve();
+
+            trail = new List<Line>();
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += ComputeCurve;
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(10);
+            dispatcherTimer.Start();
+
+            innerCircle = new Ellipse()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            outerCircle = new Ellipse()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            alpha = new Ellipse()
+            {
+                Width = 8,
+                Height = 8,
+                Fill = new SolidColorBrush(Color.FromArgb(255, 0, 150, 255))
+            };
+
+            MainCanvas.Children.Add(innerCircle);
+            MainCanvas.Children.Add(outerCircle);
+            MainCanvas.Children.Add(alpha);
+        }
+
+        private void ComputeCurve(object sender, EventArgs e)
+        {
+            curve.Compute();
+
+            tTextBox.Text = curve.t.ToString();
+
+            //outerCircle.SetValue(Canvas.LeftProperty, canvasMid.x + curve.O.x);
+            //outerCircle.SetValue(Canvas.TopProperty, canvasMid.y - curve.O.y);
+
+            //innerCircle.SetValue(Canvas.LeftProperty, canvasMid.x + curve.o.x);
+            //innerCircle.SetValue(Canvas.TopProperty, canvasMid.y - curve.o.y);
+
+            canvasMid = new Point(MainCanvas.ActualWidth / 2, MainCanvas.ActualHeight / 2);
+
+            Canvas.SetLeft(outerCircle, canvasMid.x + curve.O.x - curve.R);
+            Canvas.SetTop(outerCircle, canvasMid.y - curve.O.y - curve.R);
+
+            Canvas.SetLeft(innerCircle, canvasMid.x + curve.o.x - curve.r);
+            Canvas.SetTop(innerCircle, canvasMid.y - curve.o.y - curve.r);
+
+            Canvas.SetLeft(alpha, canvasMid.x + curve.alpha.x - alpha.Width / 2);
+            Canvas.SetTop(alpha, canvasMid.y - curve.alpha.y - alpha.Height / 2);
+
+            outerCircle.Width = 2 * curve.R;
+            outerCircle.Height = 2 * curve.R;
+
+            innerCircle.Width = 2 * curve.r;
+            innerCircle.Height = 2 * curve.r;
+
+            int trainCount = curve.trail.Count;
+            if (trainCount > 1)
+            {
+                Line newLine = new Line()
+                {
+                    X1 = canvasMid.x + curve.trail[trainCount - 2].x,
+                    Y1 = canvasMid.y - curve.trail[trainCount - 2].y,
+                    X2 = canvasMid.x + curve.trail[trainCount - 1].x,
+                    Y2 = canvasMid.y - curve.trail[trainCount - 1].y,
+                    Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 150, 255)),
+                    StrokeThickness = 2
+                };
+                trail.Add(newLine);
+                MainCanvas.Children.Add(newLine);
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void rTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Double.TryParse(rTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double r))
+            {
+                if (Double.TryParse(RTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double R))
+                {
+                    RkTextBox.Text = (R / r).ToString();
+                }
+                else if (Double.TryParse(RkTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double Rk))
+                {
+                    RTextBox.Text = (r * Rk).ToString();
+                    curve.R = r * Rk;
+                }
+                curve.r = r;
+            }
+            CheckIfStartPossible();
+        }
+
+        private void RTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Double.TryParse(RTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double R))
+            {
+                if (Double.TryParse(rTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double r))
+                {
+                    RkTextBox.Text = (R / r).ToString();
+                }
+                else if (Double.TryParse(RkTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double Rk))
+                {
+                    rTextBox.Text = (R / Rk).ToString();
+                    curve.r = R / Rk;
+                }
+                curve.R = R;
+            }
+            CheckIfStartPossible();
+        }
+
+        private void RkTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Double.TryParse(RkTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double Rk))
+            {
+                if (Double.TryParse(RTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double R))
+                {
+                    rTextBox.Text = (R / Rk).ToString();
+                    curve.r = R / Rk;
+                }
+                else if (Double.TryParse(rTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double r))
+                {
+                    RTextBox.Text = (r * Rk).ToString();
+                    curve.R = r * Rk;
+                }
+            }
+            CheckIfStartPossible();
+        }
+
+        private void SpeedTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Double.TryParse(speedTextBox.Text, out double speed) && curve != null)
+            {
+                curve.speed = speed;
+            }
+            CheckIfStartPossible();
+        }
+
+        private void CheckIfStartPossible()
+        {
+            StartButton.IsEnabled =
+                Double.TryParse(rTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double f) &&
+                Double.TryParse(RTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out f) &&
+                Double.TryParse(speedTextBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out f);
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            curve.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            curve.Stop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var line in trail)
+            {
+                if (MainCanvas.Children.Contains(line))
+                {
+                    MainCanvas.Children.Remove(line);
+                }
+            }
+            trail.Clear();
+            curve.Reset();
+        }
+
+        private void CirclesCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            innerCircle.Visibility = circlesCheckBox.IsChecked ?? false ? Visibility.Visible : Visibility.Hidden;
+            outerCircle.Visibility = circlesCheckBox.IsChecked ?? false ? Visibility.Visible : Visibility.Hidden;
         }
     }
 }
